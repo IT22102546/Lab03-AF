d03a5b930c6961b43b2e3958c0213a90645c3907
Sanjana Nimesh <sanjana.nim2001@gmail.com>
2025-03-01 18:18:33 +0530

    step two done

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ readFile('file.txt', 'utf8', function (err, data) {
  console.log(data);
 });
 
+const fs = require('fs');
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+ if (err) throw err;
+ console.log('File saved!');
+});
+
 
